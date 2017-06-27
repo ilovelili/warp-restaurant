1ea76f89d9c5193f47e80f3562b2cca7d6c475e3
mju <mju@cimpress.com>
2017-06-27 21:07:37 +0900

    sekeleton with docker compose

---
+++ server/routes/healthcheck.js
@@ -0,0 +1,10 @@
+// healthcheck.js
+const router = require('express').Router();
+
+router.get('/', (req, res, next) => {
+  return res.json({
+    healthy: true
+  });
+});
+
+module.exports = router;
---
+++ server/test/routes.js
@@ -0,0 +1,37 @@
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+
+const test = require('tape');
+
+test('GET /', t => {
+    api
+        .get('/')
+        .expect('Content-type', /json/)
+        .expect(200)
+        .end((err, res) => {
+            if (err) {
+                t.fail(err);
+                t.end();
+            } else {
+                t.ok(res.body, 'It should have a response body');
+                t.equals(res.body.healthy, true, 'It should return a healthy parameter and it should be true');
+                t.end();
+            }
+        });
+});
+
+// Ensure we get the proper 404 when trying to GET an unknown route
+test('GET unknown route', t => {
+    api
+        .get(`/${Math.random() * 10}`)
+        .expect(404)
+        .end((err, res) => {
+            if (err) {
+                t.fail(err);
+                t.end();
+            } else {
+                t.end();
+            }
+        });
+});
