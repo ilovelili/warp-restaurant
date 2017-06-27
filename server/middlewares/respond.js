29a092f4120d7d9c0895319b202beb843c64289e
mju <mju@cimpress.com>
2017-06-28 02:51:05 +0900

    allow origin

--- server/middlewares/respond.js
+++ server/middlewares/respond.js
@@ -1,7 +1,7 @@
 module.exports = () => {
     return async function respond(req, res, next) {
         res.type('json');
-        res.header("Access-Control-Allow-Origin", "*")
+        res.header("Access-Control-Allow-Origin", "http://localhost:4200")
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
         await next();
     };
