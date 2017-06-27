a63c34779dd5ca23fc7e0924bb8bf7982cc3a9ad
mju <mju@cimpress.com>
2017-06-27 21:51:24 +0900

    intergrate with mongoose

--- server/app.js
+++ server/app.js
@@ -15,7 +15,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'), { etag: false }));
 
-app.use('/', healthcheck);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+app.use('/health', healthcheck);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
